d9928d9ff12a1471a3312f3aa1e7e97e3fd0c26a
Namani <kwg531@naver.com>
2024-11-13 14:47:24 +0900

    Refactor: `a` 태그 대신 `Link` 태그로 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import menubuger from "../img/menubuger.png"
 import '../css/Header.css'
+import {Link} from "react-router-dom";
 
 function Header() {
-  return (
-    <nav className="navbar navbar-expand-lg" data-bs-theme="dark" >
-      <div className="container-fluid" >
-        {/* 중앙에 Rechelin Korea 텍스트 */}
-        <a className="navbar-brand mx-auto" >Rechelin Korea</a>
+    return (
+        <nav className="navbar navbar-expand-lg" data-bs-theme="dark">
+            <div className="container-fluid">
+                {/* 중앙에 Rechelin Korea 텍스트 */}
+                <Link className="navbar-brand mx-auto" to="/">Rechelin Korea</Link>
 
-        {/* 오른쪽 끝에 드롭다운 메뉴 */}
-        <div className="dropdown" >
-          <button className="btn" data-bs-toggle="dropdown">
-          <img src={menubuger} />
-          </button>
-          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <li><a className="dropdown-item" >Action</a></li>
-            <li><a className="dropdown-item" >Another action</a></li>
-            <li><a className="dropdown-item" >Something else here</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+                {/* 오른쪽 끝에 드롭다운 메뉴 */}
+                <div className="dropdown">
+                    <button className="btn" data-bs-toggle="dropdown">
+                        <img src={menubuger}/>
+                    </button>
+
+                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <li><Link className="dropdown-item" to="/action">Action</Link></li>
+                        <li><Link className="dropdown-item" to="/another">Another action</Link></li>
+                        <li><Link className="dropdown-item" to="/something">Something else here</Link></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
 export default Header;
