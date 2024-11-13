51e7b4261a03803bbde2fdc89709b6453d7cc8a4
Namani <kwg531@naver.com>
2024-11-13 14:46:58 +0900

    Feat: `BrowserRouter`를 추가하여 경로 설정

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,20 @@ import Footer from "./components/Footer"
 // 부트스트랩 임포트
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <ReserveMain/>
-      <Footer/>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Header/>
+                <Routes>
+                    <Route path="/" element={<ReserveMain/>}/>
+                </Routes>\
+                <Footer/>
+            </div>
+        </BrowserRouter>
+    );
 }
+
 export default App;
