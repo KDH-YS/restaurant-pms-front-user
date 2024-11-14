fef76f4063a3fe3ece041c04d4ab889ae8f37052
Jammomu <pridom03@naver.com>
2024-11-14 16:06:56 +0900

    mypage생성

---
+++ src/components/MyPage.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer"
+
+function MyPage() {
+  return (
+    <>
+      <Header/>
+      <div className="MyPage">
+        <h1>My Page</h1>
+      </div>
+      <Footer/>
+    </>
+  );
+}
