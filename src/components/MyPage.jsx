e3015d55edfe78274699899b0b20f03c364d7598
Jammomu <pridom03@naver.com>
2024-11-14 17:30:56 +0900

    마이페이지 초안

--- src/components/MyPage.jsx
+++ src/components/MyPage.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import '../css/Header.css'
 import {Link} from "react-router-dom";
+import '../css/Header.css'
 
 function MyPage() {
   return (
     <div className="MyPage">
       <h1>My Page</h1>
+
+      {/* 유저 프로필 : 사진, 이름, 프로필 수정 버튼 */}
       <div className='UserProfile'>
-        {/* 유저 프로필 사진, 이름, 프로필 수정 버튼 */}
-        {/* 개인 정보 수정 페이지로 이동 */}
         <Link className="editUserProfile" to="/editUserProfile">프로필 수정</Link>
       </div>
 
+      {/* 예약 현황 : 예약된 레스토랑, 예약 이력 */}
       <div className="Reservation">
         <p>예약 현황</p>
         <Link className="reservationHistory" to="/reservationHistory">예약 이력</Link>
       </div>
 
+      {/* 내 리뷰 : 내 리뷰 리스트, 내 리뷰 전체 보기 */}
       <div className="MyReviewList">
         <p>내 리뷰</p>
         <Link className="myReviewListAll" to="/myReviewListAll">전체보기{'>'}</Link>
@@ -29,6 +31,7 @@ function MyPage() {
         </ul>
       </div>
 
+      {/* 즐겨찾기 : 즐겨찾기 리스트, 즐겨찾기 전체보기 */}
       <div className="favorites">
         <p>즐겨찾기</p>
         <Link className="favoritesAll" to="/favoritesAll">전체보기{'>'}</Link>
