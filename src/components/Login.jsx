6ab5531f6d2dbfbc9854b5e761fe9790854829df
Jammomu <pridom03@naver.com>
2024-12-04 16:12:53 +0900

    회원가입/로그인 css 작성 및 클래스명 수정

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,36 +1,31 @@
 import React from 'react';
+import '../css/main.css';
+import '../css/login.css';
 
 function Login() {
   return (
-    <div className='HjLogin'>
-      <div className='HjLoginContent'>
-        <form className='HjLoginForm'>
-          {/* 로고 들어갈 곳(삭제해도 무관) */}
-          <img src='#'/>
-          {/* 로그인 페이지 타이틀 */}
-          <h2 className='HjLoginTitle'>Login Page</h2>
+    <div className='HjLoginContent'>
+      <form className='HjLoginForm'>
+        {/* 로고 들어갈 곳(삭제해도 무관) */}
+        <img src='#' className='HjLoginFormLogo'/>
+        {/* 로그인 페이지 타이틀 */}
+        <h2 className='HjLoginTitle'>로그인</h2>
 
-          {/* 로그인 입력칸 */}
-          <div className='HjInputBox'>
-            <p className='HjInputEmail'>
-              아이디 : 
-              <input type='email' id='HjEmail' placeholder='이메일을 입력하세요.'/>
-            </p>
-            <p className='HjInputPassword'>
-              비밀번호 : 
-              <input type='password' id='HjPassword' placeholder='비밀번호를 입력하세요.'/>
-            </p>
-          </div>
-          {/* 로그인 버튼 */}
-          <button type='submit' className='HjLoginButton'>로그인</button>
+        {/* 로그인 입력칸 */}
+        <div className='HjInputBox'>
+            <input type='email' id='HjEmail' placeholder='아이디를 입력하세요.'/>
+            <input type='password' id='HjPassword' placeholder='비밀번호를 입력하세요.'/>
+        </div>
+        {/* 로그인 버튼 */}
+        <button type='submit' className='HjLoginButton'>로그인</button>
 
-          {/* 로그인창 하단 네비(가로 정렬, li와 li 사이에 '|' 삽입) */}
-          <ul className='HjLoginNav'>
-            <li className='HjLoginNavList'>회원가입</li>
-            <li className='HjLoginNavList'>아이디/비밀번호 찾기</li>
-          </ul>
-        </form>
-      </div>
+        {/* 로그인창 하단 네비(가로 정렬, li와 li 사이에 '|' 삽입) */}
+        <ul className='HjLoginNav'>
+          <li className='HjLoginNavList'>회원가입</li>
+          <li className='HjLoginNavList'>아이디 찾기</li>
+          <li className='HjLoginNavList'>비밀번호 찾기</li>
+        </ul>
+      </form>
     </div>
   );
 }
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,30 +1,135 @@
 import React from "react";
+import '../css/main.css';
+import '../css/signup.css';
+import { useState } from "react";
 
 function Signup() {
+
+  // 모든 체크박스 상태를 관리
+  const [checkboxState, setCheckboxState] = useState({
+    HjSignupCheckAll: false,
+    HjSignupCheckAgeOlder: false,
+    HjSignupCheckTermsOfService: false,
+    HjSignupCheckPersonalInformation: false,
+    HjSignupCheckPersonalInformationSelect: false,
+    HjSignupCheckNotificationAgreed: false,
+  });
+
+  // 전체 체크박스 상태 변경
+  const handleCheckAll = (e) => {
+    const isChecked = e.target.checked;
+    setCheckboxState({
+      HjSignupCheckAll: isChecked,
+      HjSignupCheckAgeOlder: isChecked,
+      HjSignupCheckTermsOfService: isChecked,
+      HjSignupCheckPersonalInformation: isChecked,
+      HjSignupCheckPersonalInformationSelect: isChecked,
+      HjSignupCheckNotificationAgreed: isChecked,
+    });
+  };
+
+  // 개별 체크박스 상태 변경
+  const handleCheckboxChange = (e) => {
+    const { id, checked } = e.target;
+
+    const updatedState = {
+      ...checkboxState,
+      [id]: checked, // 변경된 체크박스의 상태만 업데이트
+    };
+
+    // 모든 체크박스가 체크되었는지 확인 (전체 동의 체크박스 상태 변경)
+    const allChecked = Object.keys(updatedState)
+      .filter((key) => key !== "HjSignupCheckAll") // HjSignupCheckAll 제외
+      .every((key) => updatedState[key]); // 나머지 체크박스들이 모두 체크되었는지 확인
+
+    updatedState.HjSignupCheckAll = allChecked; // 모든 체크박스가 체크되면 전체 동의 체크박스도 체크
+
+    setCheckboxState(updatedState);
+  };
+
   return (
     <div className="HjSignup">
       <div className="HjSignupContent">
         <form className="HjSignupForm" action="">
           <h2>회원가입</h2>
           <br />
-          <label htmlFor="HjSignupInputId" className="HjSignupLabel">아이디:</label>
-          <input type="text" name="userName" id="HjSignupInputId" placeholder="아이디를 입력하세요" />
-          <br />
-          <label htmlFor="HjSignupInputPassword" className="HjSignupLabel">비밀번호:</label>
-          <input type="password" name="password" id="HjSignupInputPassword" placeholder="비밀번호를 입력하세요" />
-          <br />
-          <label htmlFor="HjSignupInputName" className="HjSignupLabel">이름:</label>
-          <input type="text" name="name" id="HjSignupInputName" placeholder="이름을 입력하세요" />
-          <br />
-          <label htmlFor="HjSignupInputEmail" className="HjSignupLabel">이메일:</label>
-          <input type="email" name="email" id="HjSignupInputEmail" placeholder="이메일을 입력하세요" />
-          <br />
-          <label htmlFor="HjSignupInputPhone" className="HjSignupLabel">휴대폰 번호:</label>
-          <input type="text" name="phone" id="HjSignupInputPhone" placeholder="예: 010-1234-5678" />
-          <br />
-          <label htmlFor="HjSignupInputNotificationAgreed" className="HjSignupLabel">알림 수신 동의</label>
-          <input type="checkbox" name="notificationAgreed" id="HjSignupInputNotificationAgreed" />
+          <div className="HjSignupLabelContainer">
 
+            <label htmlFor="HjSignupInputId" className="HjSignupLabel">아이디:</label>
+            <input type="text" name="userName" id="HjSignupInputId" placeholder="아이디를 입력하세요" />
+            <br />
+
+            <label htmlFor="HjSignupInputPassword" className="HjSignupLabel">비밀번호:</label>
+            <input type="password" name="password" id="HjSignupInputPassword" placeholder="비밀번호를 입력하세요" />
+            <br />
+
+            <label htmlFor="HjSignupInputName" className="HjSignupLabel">이름:</label>
+            <input type="text" name="name" id="HjSignupInputName" placeholder="이름을 입력하세요" />
+            <br />
+
+            <label htmlFor="HjSignupInputEmail" className="HjSignupLabel">이메일:</label>
+            <input type="email" name="email" id="HjSignupInputEmail" placeholder="이메일을 입력하세요" />
+            <button type="submit" className="HjEmailSendBtn">인증번호 받기</button>
+            <input type="text" name="emailCheckNum" id="HjSignupInputEmailCheckNum" placeholder="인증 번호를 입력하세요" />
+            <button type="submit" className="HjEmailCheckBtn">인증번호 확인</button>
+            <br />
+
+            <label htmlFor="HjSignupInputPhone" className="HjSignupLabel">휴대폰 번호:</label>
+            <input type="text" name="phone" id="HjSignupInputPhone" placeholder="예: 010-1234-5678" />
+            <br />
+
+            {/* 체크박스 */}
+            <div className="HjSignupCheckBoxContainer">
+              <div>
+                <input type="checkbox" name="checkAll" id="HjSignupCheckAll" 
+                checked={checkboxState.HjSignupCheckAll} 
+                onChange={handleCheckAll} 
+                />
+                <label htmlFor="HjSignupCheckAll" className="HjSignupLabelCheckbox">전체 동의</label>
+              </div>
+
+              <div>
+                <input type="checkbox" name="notificationAgreed" id="HjSignupCheckAgeOlder" 
+                checked={checkboxState.HjSignupCheckAgeOlder} 
+                onChange={handleCheckboxChange} 
+                />
+                <label htmlFor="HjSignupCheckAgeOlder" className="HjSignupLabelCheckbox">만 14세 이상입니다.</label>
+              </div>
+           
+              <div>
+                <input type="checkbox" name="notificationAgreed" id="HjSignupCheckTermsOfService" 
+                checked={checkboxState.HjSignupCheckTermsOfService} 
+                onChange={handleCheckboxChange} 
+                />
+                <label htmlFor="HjSignupCheckTermsOfService" className="HjSignupLabelCheckbox">[필수] 서비스 이용약관에 동의</label>
+              </div>
+            
+              <div>
+                <input type="checkbox" name="notificationAgreed" id="HjSignupCheckPersonalInformation" 
+                checked={checkboxState.HjSignupCheckPersonalInformation} 
+                onChange={handleCheckboxChange} 
+                />
+                <label htmlFor="HjSignupCheckPersonalInformation" className="HjSignupLabelCheckbox">[필수] 개인정보 수집 및 이용 동의</label>
+              </div>
+           
+              <div>
+                <input type="checkbox" name="notificationAgreed" id="HjSignupCheckPersonalInformationSelect" 
+                checked={checkboxState.HjSignupCheckPersonalInformationSelect} 
+                onChange={handleCheckboxChange} 
+                />
+                <label htmlFor="HjSignupCheckPersonalInformationSelect" className="HjSignupLabelCheckbox">[선택] 개인정보 수집 및 이용 동의</label>
+              </div>
+           
+              <div>
+                <input type="checkbox" name="notificationAgreed" id="HjSignupCheckNotificationAgreed" 
+                checked={checkboxState.HjSignupCheckNotificationAgreed} 
+                onChange={handleCheckboxChange} 
+                />
+                <label htmlFor="HjSignupCheckNotificationAgreed" className="HjSignupLabelCheckbox">[선택] 마케팅 정보 수신 동의</label>
+              </div>
+            </div>
+          </div>
+          <button type="submit" className="HjSignupBtn">가입하기</button>
         </form>
       </div>
     </div>
