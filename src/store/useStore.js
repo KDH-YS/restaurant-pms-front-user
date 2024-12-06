74c813634b234e47aa0743b39ae5ece13224f740
kdh <gloian@naver.com>
2024-12-06 14:13:44 +0900

    정리

--- src/store/useStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand';
-
-// 상태 스토어 생성
-const useStore = create((set) => ({
-  count: 0, // 초기 상태 값
-  increment: () => set((state) => ({ count: state.count + 1 })), // 상태 업데이트 함수
-  decrement: () => set((state) => ({ count: state.count - 1 })),
-}));
