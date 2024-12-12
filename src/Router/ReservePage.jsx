b450bfa00af8405becfaf661b509fc89d13ad1ac
kdh <gloian@naver.com>
2024-12-12 11:30:40 +0900

    코드 정리

--- src/Router/ReservePage.jsx
+++ src/Router/ReservePage.jsx
@@ -1,6 +1,6 @@
-import Reserve from "../components/KDH/Reserve";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Reserve from "components/KDH/Reserve";
+import Header from "components/Header";
+import Footer from "components/Footer";
 // 부트스트랩 임포트
 function ReservePage() {
     return (
